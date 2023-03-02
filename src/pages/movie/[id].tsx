e8fedb6f14c9df81166972671ea3dcb925e0f910
jaussems <jannesaussems@gmail.com>
2023-03-02 11:16:12 +0100

    added pages, dynamic pages setup but needs API to fully generate paths

--- src/pages/movie/[id].tsx
+++ src/pages/movie/[id].tsx
@@ -1,9 +1,32 @@
-const id = 3333;
+import { GetStaticPaths, GetStaticProps } from "next";
+import { useRouter } from "next/router";
 
-const Movie = ({ id }) => {
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [{ params: { id: "1" } }, { params: { id: "2" } }],
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = async (params) => {
+  return {
+    props: { id: {} },
+  };
+};
+
+const Movie = () => {
+  const router = useRouter();
+  const { id } = router.query;
+
+  if (router.isFallback) {
+    return <div>Loading...</div>;
+  }
+
+  const welcomeText = `Welcome to movie page ${id}`;
+
   return (
     <>
-      <h1>Welcome to the movie page</h1>
+      <h1>{welcomeText}</h1>
     </>
   );
 };
