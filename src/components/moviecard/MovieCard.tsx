6e1b6919e7c166b737c122b05e8fa939c4ce5125
jaussems <jannesaussems@gmail.com>
2023-03-02 13:40:42 +0100

    added baseURL to movieCard to be able to generate an image

--- src/components/moviecard/MovieCard.tsx
+++ src/components/moviecard/MovieCard.tsx
@@ -2,7 +2,19 @@ import { IMovieCard } from "@/shared/models/interfaces";
 import { FC } from "react";
 
 const MovieCard: FC<IMovieCard> = (props) => {
-  return <div></div>;
+  //baseUrl can change in the future, dependable on API call configuration.
+  const baseUrl = `http://image.tmdb.org/t/p/`;
+
+  return (
+    <div>
+      <h1>{props.title}</h1>
+      <img src={`${baseUrl} + ${props.imageSource}`} />
+      <div>
+        <p>The rating is {props.rating}</p>
+      </div>
+      <div>{/* adding comment section later */}</div>
+    </div>
+  );
 };
 
 export default MovieCard;
