0435c18377a5975c0f8d74a116b4d6c6e88d6b47
jaussems <jannesaussems@gmail.com>
2023-03-02 14:11:06 +0100

    fixed images, updatd config.js to allow external host URL for image

--- next.config.js
+++ next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'http',
+        hostname: 'image.tmdb.org',
+        port: '',
+        pathname: '/t/p/w300/**',
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
--- src/components/moviecard/MovieCard.tsx
+++ src/components/moviecard/MovieCard.tsx
@@ -1,14 +1,20 @@
 import { IMovieCard } from "@/shared/models/interfaces";
 import { FC } from "react";
+import Image from "next/image";
 import styles from "../../styles/MovieCard.module.scss";
 const MovieCard: FC<IMovieCard> = (props) => {
   //baseUrl can change in the future, dependable on API call configuration.
-  const baseUrl = `http://image.tmdb.org/t/p/`;
+  const baseUrl = `http://image.tmdb.org/t/p/w300`;
 
   return (
     <div className={styles.movie_card}>
       <h1>{props.title}</h1>
-      {/* <img src={`${baseUrl}` + `${props.imageSource}`} /> */}
+      <Image
+        src={`${baseUrl}` + `${props.imageSource}`}
+        alt="Movie poster Image"
+        width={200}
+        height={400}
+      />
       <div>
         <p>The rating is {props.rating}</p>
       </div>
