014a2737528eb3377dbf3d20c4e5a78a2d2a2544
jaussems <jannesaussems@gmail.com>
2023-03-02 10:47:45 +0100

    generated page structure

---
+++ src/pages/movie/[id].tsx
@@ -0,0 +1,11 @@
+const id = 3333;
+
+const Movie = ({ id }) => {
+  return (
+    <>
+      <h1>Welcome to the movie page</h1>
+    </>
+  );
+};
+
+export default Movie;
---
+++ src/pages/profilepage.tsx
@@ -0,0 +1,9 @@
+const ProfilePage = () => {
+  return (
+    <>
+      <h1>Welcome to the profile page</h1>
+    </>
+  );
+};
+
+export default ProfilePage;
